43858c075057027bb37e9f53a19b515e184e3470
mr.zhao <about.mr.zhao@gmail.com>
2026-04-24 22:56:01 +0800

    django_video定义用户模型类，关系模型类进行数据库映射

--- users/models.py
+++ users/models.py
@@ -24,3 +24,19 @@ class UserModel(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+
+# 用户之间关注的模型类
+class UserFocusmodel(BaseModel):
+    """两个树形表达了，一个用户关注了另外一个用户"""
+    cur_user = models.ForeignKey('UserModel',verbose_name='当前用户',related_name='current_user_list',on_delete=models.CASCADE)
+    focus_user = models.ForeignKey('UserModel',verbose_name='被关注用户',related_name='focus_user_list',on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_focus'
+        verbose_name = '用户关注表'
+        verbose_name_plural = verbose_name
+        ordering = ['id']
+
+    def __str__(self):
+        return self.cur_user.username + self.focus_user.username
