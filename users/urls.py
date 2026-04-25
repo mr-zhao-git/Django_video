4a7bc835ba580c67f1d87d0102c5937a7a2b1fed
mr.zhao <about.mr.zhao@gmail.com>
2026-04-25 13:40:48 +0800

    django_video配置用户注册页面，ajax请求用户名是否唯一

--- users/urls.py
+++ users/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.template.backends.django import Template
 from django.urls import path
+from django.views.generic import TemplateView
+from .views import check_username
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('register/', TemplateView.as_view(template_name='register.html')),
+    path('check_username/', check_username),  # 通过ajax发送请求，不需要写url地址
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
+from http.client import HTTPResponse
+
+from django.db.models.expressions import result
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from users.models import UserModel
+
 
 # Create your views here.
+@require_http_methods(['POST'])  # 设置装饰器，只允许post请求
+def check_username(request):
+    """验证用户名是否唯一"""
+    # {'valid': True}:如果返回的是True，表示验证合法，通过
+    # {'valid': False}:如果返回的是False，表示验证不合法，不通过
+    result_data = {'valid': True}  # 先定义一个默认的返回参数
+    un = request.POST.get('username')  # 接收请求参数
+    user = UserModel.objects.filter(username=un)
+    if user:
+        # 已经存在一个用户名字相同的用户
+        result_data['valid'] = False
+    return JsonResponse(result_data)
